cf8c8b99dde56853727752002654c7ec59b4043f
Justen vannocker <209983489+zeroday31337x@users.noreply.github.com>
2026-02-25 08:30:03 -0500

    Update DocumentViewer.tsx

--- src/components/documents/DocumentViewer.tsx
+++ src/components/documents/DocumentViewer.tsx
@@ -4,23 +4,24 @@ import TextViewer from './TextViewer';
 import PdfViewer from './PdfViewer';
 import HtmlViewer from './HtmlViewer';
 
-export type DocumentData = {
-  type: 'text' | 'pdf' | 'html';
+type DocumentLike = {
+  format: 'pdf' | 'html' | 'text' | 'markdown';
   src: string;
 };
 
 export default function DocumentViewer({
   document,
 }: {
-  document: DocumentData;
+  document: DocumentLike;
 }) {
-  switch (document.type) {
+  switch (document.format) {
     case 'pdf':
       return <PdfViewer src={document.src} />;
 
     case 'html':
       return <HtmlViewer src={document.src} />;
 
+    case 'markdown':
     case 'text':
     default:
       return <TextViewer src={document.src} />;
