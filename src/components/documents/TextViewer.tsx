26d15b3ecd2deab65077dc5e3ad09ce3137e0bbd
Justen vannocker <209983489+zeroday31337x@users.noreply.github.com>
2026-03-27 19:14:38 -0400

    Change TextViewer to use prose for content display
    Refactor TextViewer to use a div with prose styling for better formatting of content.

--- src/components/documents/TextViewer.tsx
+++ src/components/documents/TextViewer.tsx
@@ -4,8 +4,13 @@ type Props = {
 
 export default function TextViewer({ content }: Props) {
   return (
-    <pre className="whitespace-pre-wrap font-mono text-sm leading-relaxed bg-black/40 p-6 rounded-lg border border-white/10">
-      {content}
-    </pre>
+    <div className="prose prose-invert max-w-none break-words
+      prose-p:leading-7
+      prose-headings:font-bold
+    ">
+      {content.split('\n\n').map((para, i) => (
+        <p key={i}>{para}</p>
+      ))}
+    </div>
   );
 }
