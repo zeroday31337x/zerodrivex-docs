f39758d5fae08f8200ff1be5f461eb403d14daff
Justen vannocker <209983489+zeroday31337x@users.noreply.github.com>
2026-04-07 02:56:28 -0400

    Update page.tsx

--- app/docs/[slug]/page.tsx
+++ app/docs/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { getDocumentBySlug } from '@/lib/documents'
 import { convertToHtml } from '@/lib/extract/convertToHtml'
-import UniversalHtmlViewer from '@/lib/extract/UniversalHtmlViewer'
+import UniversalHtmlViewer from '@/src/components/documents//UniversalHtmlViewer'
 
 export default async function DocPage({ params }: { params: { slug: string } }) {
   const { slug } = params
